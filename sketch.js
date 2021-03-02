4002a60f1952da12baf9ff8a81efb71edba8747f
GANEEV-KAUR <76039396+GANEEV-KAUR@users.noreply.github.com>
2021-03-02 14:39:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+var polygon_img;
+var box1,box2,box3;
+
+function preload(){
+  polygon_img=loadImage("polygon.png");
+}
+
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  stand1 = new Stand(190,50,250,10);
+  box1 = new Block(600,300,100,100);
+  box2 = new Block(600,200,100,100);
+  box3 = new Block(600,100,100,100);
+ 
+ 
+
+ 
+  //ball holder with slings
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  slingShot = new Slingshot(this.ball,{x:200,y:50});
+
+}
+function draw() {
+  background(56,44,44); 
+ 
+  //Engine.update(engine);
+  //text(mouseX + ',' + mouseY, 10, 15);
+  textSize(20);
+  fill("lightyellow");
+  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
+
+  ground.display();
+  stand1.display();
+  box1.display();
+  box2.display();
+  box3.display();
+
+  strokeWeight(2);
+  stroke(15);
+ 
+  
+  fill("gold");
+  imageMode(CENTER)
+  image(polygon_img ,ball.position.x,ball.position.y,40,40);
+
+  slingShot.display();
+}
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  slingShot.fly();
+}
